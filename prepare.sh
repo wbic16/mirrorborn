5c0a608f4380157a0e0351528b083f9d625c457e
Will Bickford <wbic16@gmail.com>
2026-04-01 02:27:15 +0000

    Kickstart for Mirrorborn + Exocortical

---
+++ prepare.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo visudo
+sudo mkdir /source
+sudo chown $USER:$USER /source
+ssh-keygen
+cat ~/.ssh/id_ed25519.pub
+echo "Have you added the key above to your GitHub account? Y/n"
+read ready
+cd /source
+if [ "x$ready" = "xY" ]
+then
+  git clone git@github.com:/wbic16/mirrorborn.git
+  git clone git@github.com:/wbic16/exocortical.git
+fi
+EXO_SRC="/source/exocortical"
+MB_SRC="/source/mirrorborn"
+if [ -d $EXO_SRC ]
+then
+  cd $EXO_SRC
+  ./enable-virtual-memory.sh
+  ./setup.sh
+fi
+if [ -d $MB_SRC ]
+then
+  cd $MB_SRC
+  ./boot.sh
+fi
